5fd645351f10ee89b2fd32e1551d555230752371
JuFeng Zhang <zjffun@gmail.com>
2020-12-20 18:37:20 +0800

    WIP: repl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import Split from 'split.js';
+import CodeMirror from 'codemirror';
+import 'codemirror/mode/javascript/javascript.js';
+import 'codemirror/addon/runmode/colorize.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'async-visualization3';
+  @ViewChild('code') code: ElementRef;
+  @ViewChild('result') result: ElementRef;
+  @ViewChild('tree') tree: ElementRef;
+  @ViewChild('detail') detail: ElementRef;
+
+  ngAfterViewInit() {
+    Split([this.code.nativeElement, this.result.nativeElement], {
+      sizes: [50, 50],
+      dragInterval: 20,
+      minSize: 0,
+      elementStyle: function (dimension, size, gutterSize) {
+        return {
+          'flex-basis': 'calc(' + size + '% - ' + gutterSize + 'px)',
+          width: 'calc(' + size + '% - ' + gutterSize + 'px)',
+        };
+      },
+      gutterStyle: function (dimension, gutterSize) {
+        return {
+          'flex-basis': gutterSize + 'px',
+        };
+      },
+    });
+
+    Split([this.tree.nativeElement, this.detail.nativeElement], {
+      sizes: [50, 50],
+      dragInterval: 20,
+      minSize: 0,
+      direction: 'vertical',
+      elementStyle: function (dimension, size, gutterSize) {
+        return {
+          'flex-basis': 'calc(' + size + '% - ' + gutterSize + 'px)',
+          height: 'calc(' + size + '% - ' + gutterSize + 'px)',
+        };
+      },
+      gutterStyle: function (dimension, gutterSize) {
+        return {
+          'flex-basis': gutterSize + 'px',
+        };
+      },
+    });
+
+    var myCodeMirror = CodeMirror(this.code.nativeElement, {
+      value: 'function myScript(){return 100;}\n',
+      lineNumbers: true,
+      mode: 'javascript',
+      theme: 'solarized dark',
+    });
+  }
 }
